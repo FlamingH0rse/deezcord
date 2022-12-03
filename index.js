a318e43a0b586bea7153a5caff448eb745ed84de
FlamingH0rse <99073459+FlamingH0rse@users.noreply.github.com>
2022-12-03 18:30:46 +0530

    electron init

--- index.js
+++ index.js
@@ -1,49 +1,39 @@
-const botconfig = require('./config.json');
-const fs = require('fs')
-const readline = require('readline')
+const { app, BrowserWindow, ipcMain, dialog } = require('electron')
+const path = require('path')
 
-const sound = require('sound-play')
-//discord:
-const Discord = require('discord.js');
-const { Client, GatewayIntentBits } = require('discord.js');
-const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildMessageReactions, GatewayIntentBits.MessageContent] });
+let mainWindow
 
-client.commands = new Discord.Collection()
-
-const slashCommandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
-for (const file of slashCommandFiles) {
-    const command = require(`./commands/${file}`)
-    client.commands.set(command.data.name, command);
+const createWindow = () => {
+    mainWindow = new BrowserWindow({
+        backgroundColor: '#212A47',
+        width: 1000,
+        height: 600,
+        minWidth: 800,
+        minHeight: 600,
+        titleBarStyle: 'hidden',
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js')
+        }
+    })
+    mainWindow.loadFile('./frontend/index.html')
 }
 
-const rl = readline.createInterface({input: process.stdin, output: process.stdout})
-let currentChannelId = botconfig.channelId
-let currentGuildId = botconfig.guildId
-client.once('ready', async () => {
-    console.log('Connected to Discord')
-    client.user.setActivity("My Slave", {
-        type: "PLAYING",
-        url: "https://www.lol.com"
-    });
-    /*
-    let owner = await client.users.fetch(botconfig.ownerID)
-    if (owner) {
-        client.user.setUsername(owner.username)
-        client.user.setAvatar(owner.displayAvatarURL())
-    }
-    */
-    let currentGuild = client.guilds.cache.get(currentGuildId)
-    let currentChannel = currentGuild.channels.cache.get(currentChannelId)
+app.whenReady().then(async () => {
+    createLoadingWindow()
 
-    rl.on('line', msg => {
-        if (msg && msg.length < 2000) currentChannel.send(msg)
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow()
     })
 })
 
-client.on('messageCreate', async message => {
-    if (message.author.bot) return
-    sound.play('D:\\Projects\\discord\\message.wav', 1)    
-    console.log(`\n${message.author.username} > ${message.content}`)
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit()
 })
 
-client.login(botconfig.token)
+process.on('uncaughtException', err => {
+    console.log(err)
+})
+
+process.on('unhandledRejection', err => {
+    console.log(err)
+})
