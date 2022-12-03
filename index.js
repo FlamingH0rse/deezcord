3221e8f453f84252e4c1fea2a8d0f610e3b3fbb4
FlamingH0rse <99073459+FlamingH0rse@users.noreply.github.com>
2022-12-04 00:32:17 +0530

    guilds list done

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
+const botconfig = require('./config.json');
+const savedcache = require('./cache.json')
 const { app, BrowserWindow, ipcMain, dialog } = require('electron')
+
+//discord:
+const Discord = require('discord.js');
+const { Client, GatewayIntentBits } = require('discord.js');
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildMessageReactions] });
+
 const path = require('path')
 
 let mainWindow
 
 const createWindow = () => {
+    console.log('Creating window...')
     mainWindow = new BrowserWindow({
         backgroundColor: '#202225',
         width: 1000,
@@ -19,10 +28,31 @@ const createWindow = () => {
     mainWindow.loadFile('./frontend/app/app.html')
 }
 
+const connectDiscord = () => {
+    return new Promise(res => {
+        client.once('ready', () => {
+            console.log('Connected to Discord');
+            client.user.setActivity("DiscordForBots", {
+                type: "PLAYING",
+                url: "https://www.github.com/FlamingH0rse"
+            });
+            res()
+        });
+    })
+}
 app.whenReady().then(async () => {
+    await connectDiscord()
     createWindow()
-
     ipcMain.handle('backend', (event, d) => {
+        console.log(d)
+        if (d.title == 'initInfo') {
+            let res = {}
+            res.guilds = client.guilds.cache.map(g => [g.name, g.id, g.iconURL()])
+            if (savedcache.lastGuild.type == 'GUILD' && client.guilds.cache.get(savedcache.lastGuild.id)) {
+                res.currentGuild = client.guilds.cache.get(savedcache.lastGuild.id)
+            }
+            return res
+        }
         //client related stuff
         if (d.title == 'minimizeWin') mainWindow.minimize()
         else if (d.title == 'maximizeWin') {
@@ -37,7 +67,6 @@ app.whenReady().then(async () => {
         }
         else if (d.title == 'closeWin') mainWindow.close()
     })
-
     app.on('activate', () => {
         if (BrowserWindow.getAllWindows().length === 0) createWindow()
     })
@@ -54,3 +83,4 @@ process.on('uncaughtException', err => {
 process.on('unhandledRejection', err => {
     console.log(err)
 })
+client.login(botconfig.token)
--- preload.js
+++ preload.js
@@ -5,7 +5,6 @@ const toBackend = async obj => {
 }
 
 let html = {}
-
 function runOnceRefreshHtmlElements() {
     let elms = document.getElementsByTagName('*')
     for (i in elms) {
@@ -13,10 +12,30 @@ function runOnceRefreshHtmlElements() {
         else if (elms[i].classList?.length) html[elms[i].classList[0]] = elms[i]
     }
     html.minimize.addEventListener('click', () => toBackend({ title: 'minimizeWin' }))
-    html.maximize.addEventListener('click', async () => { let res = await toBackend({ title: 'maximizeWin' })})
+    html.maximize.addEventListener('click', async () => { let res = await toBackend({ title: 'maximizeWin' }) })
     html.close.addEventListener('click', () => toBackend({ title: 'closeWin' }))
 }
 
+function renderGuild(g) {
+    let newGuild = document.createElement('div')
+    newGuild.classList.add('guild')
+    newGuild.innerHTML =
+        `<img class="guildIcon" src="${g[2]}"></img>
+    <div class="guildName" style="display: none;">${g[0]}</div>`
+    html.serverslist.append(newGuild)
+    runOnceRefreshHtmlElements()
+    newGuild.children[0].addEventListener('click', e => toBackend({ title: 'navGuild', type: 'GUILD', id: g[1] }))
+}
+
+ipcRenderer.on('frontend', (event, d) => {
+    console.log(d)
+})
 window.addEventListener('load', async () => {
     runOnceRefreshHtmlElements()
+    if (window.location.href.split('/').pop() == 'app.html') {
+        let initInfo = await toBackend({ title: 'initInfo' })
+        for (let g in initInfo.guilds) {
+            renderGuild(initInfo.guilds[g])
+        }
+    }
 })
