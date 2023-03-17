5e9ae415c27cf8bbeebfeb9c33a75cf7fa38b118
FlamingH0rse <99073459+FlamingH0rse@users.noreply.github.com>
2023-03-17 14:21:52 +0530

    export functions

--- preload.js
+++ preload.js
@@ -1,8 +1,7 @@
 const { ipcRenderer } = require('electron')
-
-const toBackend = async obj => {
-    return await ipcRenderer.invoke('backend', obj)
-}
+const path = require('path')
+let { renderChannel, renderGuild } = require(path.join(window.location.pathname.slice(1), '..', '..', 'js', 'renderer.js'))
+let { toBackend } = require(path.join(window.location.pathname.slice(1), '..', '..', 'js', 'electronipc.js'))
 
 let html = {}
 let titleBarInit = false
@@ -23,39 +22,6 @@ function runOnceRefreshHtmlElements() {
     }
 }
 
-function renderChannel(c) {
-    if (c[0] == 0) {
-        let newChannel = document.createElement('div')
-        newChannel.classList.add('channel')
-        newChannel.innerHTML =
-            `<img class="channelIcon" src="../assets/channel.svg">
-            <p class="channelName" id="${c[2]}">${c[1]}</p>`
-        html.channelslist.append(newChannel)
-    } if (c[0] == 4) {
-        let newCategory = document.createElement('div')
-        newCategory.classList.add('category')
-        newCategory.innerHTML =
-            `<p class="categoryName" id="${c[2]}">${c[1].toUpperCase()}</p>`
-        html.channelslist.append(newCategory)
-    }
-    runOnceRefreshHtmlElements()
-}
-
-function renderGuild(g) {
-    let newGuild = document.createElement('div')
-    newGuild.classList.add('guild')
-    newGuild.innerHTML =
-        `<img class="guildIcon" src="${g[2]}"></img>
-        <div class="guildName" style="display: none;">${g[0]}</div>`
-    html.serverslist.append(newGuild)
-    runOnceRefreshHtmlElements()
-    newGuild.children[0].addEventListener('click', async e => {
-        let guildInfo = await toBackend({ title: 'navGuild', type: 'GUILD', id: g[1] })
-        html.channelslist.innerHTML = ''
-        html.channeltop.textContent = g[0]
-        guildInfo.d.channels.forEach(c => { renderChannel([c[1].type, c[1].name, c[1].id]) })
-    })
-}
 ipcRenderer.on('frontend', (event, d) => {
     console.log(d)
 })
@@ -67,7 +33,8 @@ window.addEventListener('load', async () => {
             `<img class="botIcon" src="${initInfo.user[1]}"></img>
             <div class="botName">${initInfo.user[0]}</div>`
         for (let g in initInfo.guilds) {
-            renderGuild(initInfo.guilds[g])
+            renderGuild(html, initInfo.guilds[g])
+            runOnceRefreshHtmlElements()
         }
     }
 })
