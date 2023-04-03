3969d822bfc072663c39b1377f8a7f9cbfd85661
flightofnoobs <42741758+flightofnoobs@users.noreply.github.com>
2023-04-03 15:52:44 +0800

    a

--- frontend/app/script.js
+++ frontend/app/script.js
@@ -1,7 +1,18 @@
 $(() => {
+    // Messy code (needs to be refactored)
+    if ($('.rightside').is(":visible")) {
+        $('#memberlisttoggle').css("background-color", "white")
+    }    
+    
     $("#loadOverlay").css("display", "none");
     $(".status").hide()
     $('#memberlisttoggle').click(() => {
         $('.rightside').toggle()
+        // Messy code (needs to be refactored)
+        if ($('.rightside').is(":visible")) {
+            $('#memberlisttoggle').css("background-color", "white")
+        } else {
+            $('#memberlisttoggle').css("background-color", "#B5BAC1")
+        }
     })
 })
