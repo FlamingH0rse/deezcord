b14bbd5e38f41337eabffc05577f9a17ab85700a
flightofnoobs <42741758+flightofnoobs@users.noreply.github.com>
2023-05-22 18:07:10 +0800

    code cleanup

--- frontend/app/script.js
+++ frontend/app/script.js
@@ -11,9 +11,9 @@ $(() => {
         if ($('.rightside').is(":visible")) $('#memberlisttoggle').css("background-color", "white")
         else $('#memberlisttoggle').css("background-color", "#B5BAC1")
     })
-    $('body').on('mouseenter', '.rolemention', function () {
+    $('body').on('mouseenter', '.rolemention', () => {
         $(this).css('background-color', $(this).css('background-color').replace('0.1', '0.3'));
-    }).on('mouseleave', '.rolemention', function () {
+    }).on('mouseleave', '.rolemention', () => {
         $(this).css('background-color', $(this).css('background-color').replace('0.3', '0.1'));
     })
 })
